2c31c40871d9bbfff8f5d6eab5c9d4d1276de565
Melissa Patterson <melissa.patterson.va@gmail.com>
2015-01-19 14:23:28 -0600

    add many to many relationship with users and videos

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,4 @@
-class Video < ActiveRecord::base
-  belongs_to :user
+class Video < ActiveRecord::Base
+  has_many :user_videos
+  has_many :users, through: :user_videos
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150119173045) do
+ActiveRecord::Schema.define(version: 20150119195904) do
+
+  create_table "user_videos", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "video_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "user_videos", ["user_id"], name: "index_user_videos_on_user_id"
+  add_index "user_videos", ["video_id"], name: "index_user_videos_on_video_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
