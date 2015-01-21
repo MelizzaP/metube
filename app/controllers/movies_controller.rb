fe563f18463a1381f44cf9c2da4d503ab2489f7e
Melissa Patterson <melissa.patterson.va@gmail.com>
2015-01-21 17:09:13 -0600

    add matilda page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,17 @@
 class MoviesController < ApplicationController
   
   def show_gladiator
+    @title = 'Gladiator'
+    @description = 'A movie that I am not that into.'
+    @subtitle = 'Russel Crowe is in this'
+    @video_id = 1
   end
   
   def show_matilda
+    @title = 'Matilda'
+    @description = 'This girl is magical'
+    @subtitle = 'A classic'
+    @video_id = 2
   end
   
 end
